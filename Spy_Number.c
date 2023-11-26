c91ad15257769a97577e4b3fc4adcaac02077b78
sarathchandrapatruni <147487777+sarathchandrapatruni@users.noreply.github.com>
2023-11-26 10:34:00 +0530

    #include<stdio.h> int main() {     int a,b,c=0,d=1;     scanf("%d",&a);     while(a>0)     {         b=a%10;         c=c+b;         d=d*b;         a=a/10;     }     if(c==d)     printf("Spy Number");     else     printf("Not Spy Number"); }

---
+++ Spy_Number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c=0,d=1;
+    scanf("%d",&a);
+    while(a>0)
+    {
+        b=a%10;
+        c=c+b;
+        d=d*b;
+        a=a/10;
+    }
+    if(c==d)
+    printf("Spy Number");
+    else
+    printf("Not Spy Number");
+}
